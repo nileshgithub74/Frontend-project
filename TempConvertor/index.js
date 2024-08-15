14daad7ab08e179ce4a19f528c4fadaf6c125df7
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-08-15 05:34:22 +0000

    javascript project

---
+++ TempConvertor/index.js
@@ -0,0 +1,30 @@
+
+const textbox = document.getElementById("textbox");
+const tofahrenheit = document.getElementById("tofahrenheit");
+const toCelsius = document.getElementById("toCelsius");
+
+const result = document.getElementById("result");
+let temp;
+
+
+function convert(){
+    if(tofahrenheit.checked){
+        temp = Number(textbox.value);
+        temp = temp *   9/5+32;
+        result.textContent=temp.toFixed(2) +   "°F";
+        
+
+
+    }else if(toCelsius.checked){
+        temp = Number(textbox.value);
+        temp = (temp -32) * (5/9);
+        result.textContent=temp.toFixed(2) +   "°C";
+
+
+    }else{
+        result.textContent=`select the unit`;
+    }
+
+
+}
+
